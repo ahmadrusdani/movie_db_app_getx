aea391e3e1167a10a66da21853253294758f0527
AhmadRusdani <ahmad.rusdani@kanggo.id>
2025-06-20 11:33:14 +0700

    remove unused link

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -16,11 +16,4 @@ class Constants {
   static const String posterUrl = 'https://image.tmdb.org/t/p/w185';
   static const String backdropUrl = 'https://image.tmdb.org/t/p/original';
 
-  //TVs URLs
-  static const String latestTv = '/tv/latest';
-  static const String airingTodayTv = '/tv/airing_today';
-  static const String onTheAirTv = '/tv/on_the_air';
-  static const String popularTv = '/tv/popular';
-  static const String topRatedTv = '/tv/top_rated';
-
 }
